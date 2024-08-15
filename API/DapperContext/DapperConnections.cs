c807c8d8c1201a7b5557c2ad4ddd76c4bbae2dab
RealAbu0 <105455070+RealAbu0@users.noreply.github.com>
2024-08-15 21:21:54 +0100

    Added dapper context folder
    I did not use Entity Framework, instead I used a Micro-ORM called Dapper. This just connects the project to the database.

---
+++ API/DapperContext/DapperConnections.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperContext
+{
+    public class DapperConnections
+    {
+        public IConfiguration _config;
+
+        private readonly string _connectionString;
+
+        public DapperConnections(IConfiguration config)
+        {
+            _config = config;
+            _connectionString = _config.GetConnectionString("Default");
+        }
+
+        public IDbConnection GetConnectionString()
+            => new SqlConnection(_connectionString);
+    }
+}
