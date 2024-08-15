68ad729e7c934f4e91c448e3e47ddf51081a53ba
RealAbu0 <105455070+RealAbu0@users.noreply.github.com>
2024-08-15 20:20:06 +0100

    Added connections
    This repository makes it easier to attain data access code and its easier to maintain and test. This gets the data from the database.

---
+++ Connections/Repositories/CardDetailsRepository.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Interface;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connections.Repositories
+{
+    public class CardDetailsRepository : ICardDetailsRepository
+    {
+        private List<CardDetails> _cards = new List<CardDetails>();
+        //{
+        //    new CardDetails
+        //    {
+        //        CardHolderName =  "abu",
+        //        CardNumber = 483984938,
+        //        CardMM = 44, CardYY = 00,
+        //        CardCVV = 123
+        //    }
+        //};
+
+       
+
+        public void AddCard(CardDetails cardDetails)
+        {
+            
+
+            _cards.Add(cardDetails);
+
+
+           
+        }
+
+        public List<CardDetails> GetAllCards()
+        {
+            return _cards;
+        }
+    }
+}
