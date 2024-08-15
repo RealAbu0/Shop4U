dec5d13027bf68a09005a8621a2c9cee043dc887
RealAbu0 <105455070+RealAbu0@users.noreply.github.com>
2024-08-15 20:24:25 +0100

    Added connections folder
    This repository makes it easier to attain data access code and its easier to maintain and test. This gets the data from the database.

---
+++ API/Connections/Repositories/ItemRepository.cs
@@ -0,0 +1,63 @@
+﻿using Dapper;
+using DapperContext;
+using Domain.Interface;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connections.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        private readonly DapperConnections _connection;
+
+        public ItemRepository(DapperConnections connections)
+        {
+            _connection = connections;
+        }
+
+        public async Task<IEnumerable<Item>> GetAllCandy()
+        {
+            var query = "GetAllCandy_sp";
+            DynamicParameters dp = new DynamicParameters();
+
+            using(var conn = _connection.GetConnectionString())
+            {
+                var result = await conn.QueryAsync<Item>(query, dp, commandType: CommandType.StoredProcedure);
+                return result.ToList();
+            }
+        }
+
+        public async Task<IEnumerable<Item>> GetAllItems()
+        {
+            var query = "GetAllRandomProducts_sp";
+
+            DynamicParameters dp = new DynamicParameters();
+
+            using(var conn = _connection.GetConnectionString())
+            {
+                var result = await conn.QueryAsync<Item>(query, dp, commandType: CommandType.StoredProcedure);
+                return result.ToList();
+            }
+        }
+
+        public async Task<IEnumerable<Item>> GetAllSoftDrinks()
+        {
+            var query = "GetAllSoftDrinks_sp";
+            DynamicParameters dp = new DynamicParameters();
+         
+
+            using( var conn = _connection.GetConnectionString())
+            {
+                var result = await conn.QueryAsync<Item>(query, dp, commandType: CommandType.StoredProcedure);
+                return result.ToList();
+            }
+        }
+    }
+}
+
+//This repository makes it easier to attain data access code and its easier to maintain and test. This gets the data from the database.
